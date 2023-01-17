7f81600f5bd270c46d2775dbe7f0943373dd1a08
Aamir Zaheer <aamirzaheer95@gmail.com>
2023-01-17 21:22:36 +0530

    Ui Changes

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -17,7 +17,7 @@ const firebaseConfig =  {
     firebase.initializeApp(firebaseConfig)
 
 
-export const auth =  firebase.auth()
+export const auth: any =  firebase.auth()
 export const googleAuthProvider = new firebase.auth.GoogleAuthProvider()
 
 export const firestore = firebase.firestore()
